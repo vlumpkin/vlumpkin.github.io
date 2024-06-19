2ca2f8e7d041007dd514929d46f917f7d13ba7a8
vlumpkin <vmoose@outlook.com>
2024-06-19 11:53:52 -0700

    initial commit

---
+++ public/header.js
@@ -0,0 +1,8 @@
+function header() {
+    return `
+        <ul class="header">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="portfolio.html">Portfolio</a></li>
+        </ul>
+        `;
+}
