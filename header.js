71d64a7d437f10050f6750a9dfc7f80e2fdae1ae
vlumpkin <vmoose@outlook.com>
2024-08-17 12:38:04 -0700

    removed files

--- header.js
+++
@@ -1,8 +0,0 @@
-function header() {
-    return `
-        <ul class="header">
-        <li><a href="index.html">Home</a></li>
-        <li><a href="portfolio.html">Portfolio</a></li>
-        </ul>
-        `;
-}
